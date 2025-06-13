4b8772f8d1b557e210ab21308871b8db96dc2f25
no1choi <pooh05307@gmail.com>
2025-06-13 15:45:01 +0900

    자동로그아웃

--- js_basic/scripts/timer_3.js
+++ js_basic/scripts/timer_3.js
@@ -27,12 +27,53 @@ let num = 0;
 // 480 = 480*1
 // 960 = 480*2
 // ... 이렇게 이동한다
+
 const productTimer = setInterval(()=>{
     //12345012345012345...
     num++
     if(num>5){num=0};
     for(let i of slideContainer){
         i.style.transform = `translateX(-${num*480}px)`
+        i.style.transition = 'transform 1s'
     }
-    console.log(num)
-},500)
+    // console.log(num)
+},2000)
+
+// 동기와 비동기 출력 연습
+console.log('A');
+console.log('B');
+console.log('C');
+//(위) 동기 작업은 순차적으로 콜스택에 쌓이며 실행된다.
+
+// console.log('A');
+// setTimeout(()=>{
+//     console.log('B');
+// },2000)
+// console.log('C');
+//(위) 동기(a,c)와 비동기(b)는 순차적으로 실행되지 않는다.
+// 동기작업 (a,c)는 콜스텍에 쌓이면 즉시 실행되고
+// 비동기작업(b)는 콜백 큐에 저장되고 준비가 되면 순차적으로 순서 상관없이 독립적으로 실행된다.
+
+//clearInterval(setInterval 변수명)
+// 카운트다운 버튼 클릭 시 10초 카운트 다운 시작
+// 1초 간격으로 10 -> 9 -> 8 ... 숫자가 감소되고 0초가 디면 타이머가 멈추고 '이벤트 종료' 메세지 출력
+
+const saleTimer = document.querySelector('.sale_timer em');
+const saleBtn = document.querySelector('#timer_btn');
+console.log(saleTimer, saleBtn); 
+
+//함수명 startCountDownFunc
+function startCountDownFunc(){
+    let num = 10;
+    const startCountDown = setInterval(()=>{
+        if(num < 0){
+            clearInterval(startCountDown); //타이머종료
+            console.log('종료!')
+        }else{
+            console.log(num);
+            saleTimer.textContent = num
+            num--;
+        }
+    },1000)
+}
+saleBtn.addEventListener('click',startCountDownFunc)
